3d5aeb8fc82df5c2c66677db0ac7260ed6c6e020
TheMasterOfMagic <18811569079@163.com>
2019-04-02 15:03:58 +0800

    完善了获取随机域名的按钮的功能

--- VisualDNS/main.py
+++ VisualDNS/main.py
@@ -1,4 +1,5 @@
 import os
+from random import choice
 from flask import request, send_file
 from flask_api import FlaskAPI
 from query import query
@@ -12,6 +13,16 @@ def api_query():
 	dns_server = request.args.get('dns_server')
 	qtype = request.args.get('qtype')
 	return query(qname, dns_server, qtype)
+
+
+with open('static/alexa_100k.txt', 'r') as f:
+	qname_list = f.readlines()
+	qname_list = list(map(str.strip, qname_list))
+
+
+@app.route('/api/random_qname')
+def apt_random_qname():
+	return dict(qname=choice(qname_list))
 
 
 @app.route('/<path:path>')
